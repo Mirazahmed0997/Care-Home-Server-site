4bbf522d5cc866152a20a6468faabde4114363b7
Mirazahmed0997 <ahmedmiraz87@.com>
2023-10-16 18:31:58 +0600

    adminApi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const cors=require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt= require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -26,11 +27,35 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+function varifyJWT(req,res,next)
+{
+  console.log('jwt', req.headers.authorization)
+  const authHeader=req.headers.authorization;
+  if(!authHeader)
+  {
+    return res.status(401).send('Unauthorised Access');
+  }
+  const token= authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN,function(err,decoded)
+  {
+    if(err)
+    {
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
+
+
 async function run() {
   try {
 
         const appointmentOptionsCollection = client.db('careHome').collection('appointmentOptions')
         const bookingCollections= client.db('careHome').collection('bookings')
+        const userCollections= client.db('careHome').collection('users')
         //user aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions',async(req,res)=>
         {
@@ -68,11 +93,101 @@ async function run() {
         // app.patch('/bookingd/:id') to update a specific booking data
         // app.delete('/bookingd/:id') to delete a specific booking data
 
+        app.get('/bookings',varifyJWT,async(req,res)=>
+        {
+          const email = req.query.email;
+          const decodedEmail =req.decoded.email; 
+          if(email!==decodedEmail)
+          {
+            return res.status(403).send({message: 'forbidden access'})
+          }
+          const query= {email:email}
+          const bookings= await bookingCollections.find(query).toArray();
+          res.send(bookings)
+        })
+
+
         app.post('/bookings',async(req,res)=>
         {
             const booking= req.body;
+            const query={
+              email:booking.email,
+              appointmentDate:booking.appointmentDate,
+              treatment:booking.treatment
+            }
+
+            const bookingCount = await bookingCollections.find(query).toArray();
+            if(bookingCount.length)
+            {
+              const message = `Already have a booking on ${booking.appointmentDate}`
+              return res.send({acknowledged:false,message})
+            }
+
             const result =await bookingCollections.insertOne(booking);
             res.send(result)
+        })
+
+
+
+        app.get('/jwt',async(req,res)=>
+        {
+          const email= req.query.email
+          const query={email:email}
+          const user= await userCollections.findOne(query)
+          if(user)
+          {
+            const token=jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn:'1h'})
+            return res.send({accessToken:token})
+          }
+          console.log(user)
+          res.status(403).send({accessToken:''})
+        })
+
+        app.get('/users',async (req,res)=>
+        {
+          const query={}
+          const users=await userCollections.find(query).toArray()
+          res.send(users)
+        })
+
+
+        app.get('/users/admin/:email', async (req,res)=>
+        {
+          const email =req.params.email;
+          const query={email}
+          const user = await userCollections.findOne(query)
+          res.send({isAdmin: user?.role=='admin'});
+        })
+
+
+        app.post('/users',async(req,res)=>
+        {
+          const user=req.body;
+          const result= await userCollections.insertOne(user)
+          res.send(result)
+        })
+
+        app.put('/users/admin/:id',varifyJWT,async(req,res)=>
+        {
+          const decodedEmail=req.decoded.email
+
+          const query={email:decodedEmail};
+          const user=await userCollections.findOne(query)
+          if(user?.role !=='admin')
+          {
+            return res.status(403).send({message: 'forbidden access'})
+          }
+
+          const id=req.params.id;
+          const filter={_id: new ObjectId(id)}
+          const options={upsert:true};
+          const updatedDoc={
+            $set:{
+              role:'admin'
+            }
+          }
+          const result= await userCollections.updateOne(filter,updatedDoc,options)
+          res.send(result)
         })
 
         // app.get('/bookings',async (req,res)=>
